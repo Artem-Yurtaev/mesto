83ae38e8ce52915f3ede6d1f8a4a0bb1517f1210
Artem Yurtaev <nefedovignacy@yandex.ru>
2022-07-11 19:33:24 +0300

    Внести правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,30 @@
 let popupElement = document.querySelector('.popup');
 let popupCloseButton = popupElement.querySelector('.popup__close');
 let popupOpenButtonElement = document.querySelector('.profile__edit-button');
-
-
-function togglePopapVisibility () {
-  popupElement.classList.toggle('popup_opened');
-}
-
-popupOpenButtonElement.addEventListener('click', togglePopapVisibility);
-popupCloseButton.addEventListener('click', togglePopapVisibility);
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__description');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_description');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
-function resetPopup () {
+
+function openPopap () {
+  popupElement.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-popupCloseButton.addEventListener('click', resetPopup);
+function closePopap () {
+  popupElement.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglePopapVisibility();
+  closePopap();
 }
 
+popupOpenButtonElement.addEventListener('click', openPopap);
+popupCloseButton.addEventListener('click', closePopap);
 formElement.addEventListener('submit', formSubmitHandler);
